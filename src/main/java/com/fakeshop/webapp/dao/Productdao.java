86b20e38fe113a8dcc5061bbae753bec7c0c6439
Chris Corday <corday@gmail.com>
2018-03-13 18:58:35 +0000

    Set up product Entity

---
+++ src/main/java/com/fakeshop/webapp/dao/Productdao.java
@@ -0,0 +1,10 @@
+package com.fakeshop.webapp.dao;
+
+import com.fakeshop.webapp.model.Product;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface Productdao extends CrudRepository<Product, Long>{
+
+}
