e7261fe390f9f3f6b5f62b949f370dfc6bb191da
Chris Corday <corday@gmail.com>
2018-04-30 13:02:39 +0100

    Homepage styling and switch to file based database

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,3 @@
-insert into Product(id, name, description, price, picture, FOR_SALE) values(1, 'Phone', 'This is a phone', 2.99, null, true);
-
 insert into Role(name) values ('ROLE_CUSTOMER'), ('ROLE_ADMIN');
 
 insert into User(id, email, name, role_id, password, is_Non_Expired, is_Non_Locked, is_Enabled) values (1, 'test@test.com', 'Chris', 1,'$2a$10$XYeP/Nk0k7Tw/fE.Lp7z9.aYyxD2sgumh7IzIs4jeEA2GsqDavJgi', true, true, true);
